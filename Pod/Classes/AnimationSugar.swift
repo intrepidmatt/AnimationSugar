9ded4b90b7be37f7cd5f2ae847cca411149f6de9
Matt Bridges <matt@intrepid.io>
2016-04-26 12:12:00 -0400

    Add UIView helper methods and documentation

--- Pod/Classes/AnimationSugar.swift
+++ Pod/Classes/AnimationSugar.swift
@@ -8,12 +8,25 @@
 import Foundation
 import UIKit
 
-public func animate(duration duration: NSTimeInterval, animations: () -> ()) -> Animation {
+/**
+ Animate changes to one or more `UIView` objects.
+ 
+ - Parameter duration: The length over which the animation(s) occur.
+ - Parameter animations: A block containing changes to `UIView` animatable properties.
+ - Returns An `Animation` object that can be further modified (e.g. changing an
+ animation curve) or chained with further animations.
+ */
+public func animate(duration duration: NSTimeInterval = Animation.defaultAnimationDuration, animations: () -> ()) -> Animation {
     return Animation(duration: duration, animations: animations)
 }
 
 public class Animation {
     
+    private struct Constants {
+        static let DefaultAnimationDuration: NSTimeInterval = 0.3
+    }
+    
+    public static var defaultAnimationDuration = Constants.DefaultAnimationDuration
     private let animations: () -> ()
     private let duration: NSTimeInterval
     private var delay: NSTimeInterval = 0
@@ -24,6 +37,14 @@ public class Animation {
     private var prevAnimation: Animation?
     private var nextAnimation: Animation?
     
+    /**
+     Initialize an `Animation` object.
+     
+     - Parameter duration: The length over which the animation(s) occur
+     - Parameter animations: A block containing changes to `UIView` animatable properties.
+     - Parameter startNow: A boolean indicating whether to immediately start the animation
+     or wait to be triggered later. Chained animations set this parameter to false.
+     */
     init(duration: NSTimeInterval, animations: () -> (), startNow: Bool = true) {
         self.duration = duration
         self.animations = animations
@@ -35,6 +56,12 @@ public class Animation {
         }
     }
     
+    /**
+     Modify the animation options.
+     
+     - Parameter option: The option(s) to set. E.g. `.withOption(.AnimationCurveEaseOut)`
+     - Returns An `Animation` object that can be modified or chained with further animations.
+     */
     public func withOption(option: UIViewAnimationOptions) -> Animation {
         if let options = options {
             self.options = options.union(option)
@@ -45,23 +72,60 @@ public class Animation {
         return self
     }
     
+    /**
+     Add a delay before an animation begins.
+     
+     - Parameter delay: The amount of time to delay.
+     - Returns An `Animation` object to be modified or chained with further animations.
+     */
     public func withDelay(delay: NSTimeInterval) -> Animation {
         self.delay = delay
         return self
     }
     
+    /**
+     Set a completion block that runs when the animation has completed.
+     
+     - Parameter completion: A block of code to run.
+     - Returns An `Animation` object to be modified or chained with further animations.
+     */
     public func withCompletion(completion: (Bool) -> ()) -> Animation {
         self.completion = completion
         return self
     }
     
+    /**
+     Animate using "spring" physics.
+     
+     - Parameter dampingRatio: The damping ratio for the spring animation as it approaches
+     its quiescent state.
+     
+     To smoothly decelerate the animation without oscillation, use a value of 1. Employ
+     a damping ratio closer to zero to increase oscillation.
+     - Parameter initialVelocity: The initial spring velocity. For smooth start to the
+     animation, match this value to the view’s velocity as it was prior to attachment.
+     A value of 1 corresponds to the total animation distance traversed in one second.
+     
+     For example, if the total animation distance is 200 points and you want the start of the
+     animation to match a view velocity of 100 pt/s, use a value of 0.5.
+     - Returns An `Animation` object to be modified or chained with further animations.
+     */
     public func withSpring(dampingRatio dampingRatio: CGFloat, initialVelocity: CGFloat) -> Animation {
         self.springDampingRatio = dampingRatio
         self.springInitialVelocity = initialVelocity
         return self
     }
     
-    public func thenAnimate(duration duration: NSTimeInterval, animations: () -> ()) -> Animation {
+    /**
+     Chain a new animation to begin immediately after a previous animation finishes.
+     
+     Use this method to prevent excessive nesting of animations inside completion blocks.
+     
+     - Parameter duration: The length over which the animation occurs.
+     - Parameter animations: A block containing changes to `UIView` animatable properties.
+     - Returns An `Animation` object to be modified or chained with further animations
+     */
+    public func thenAnimate(duration duration: NSTimeInterval = defaultAnimationDuration, animations: () -> ()) -> Animation {
         let nextAnimation = Animation(duration: duration, animations: animations, startNow: false)
         nextAnimation.prevAnimation = self
         self.nextAnimation = nextAnimation
@@ -77,6 +141,9 @@ public class Animation {
         return nextAnimation
     }
     
+    /**
+     Start an animation.
+     */
     private func start() {
         if let prevAnimation = self.prevAnimation {
             prevAnimation.start()
@@ -101,6 +168,127 @@ public class Animation {
                 options: options ?? [],
                 animations: animations,
                 completion: completion)
+        }
+    }
+}
+
+extension UIView {
+    /**
+     Translate the view using its `transform` property.
+     
+     - Parameter tx: Translation distance along the x axis
+     - Parameter ty: Translation distance along the y axis
+     */
+    func translate(tx tx: CGFloat, ty: CGFloat) {
+        self.transform = CGAffineTransformTranslate(self.transform, tx, ty)
+    }
+    
+    /**
+     Scale the view using its `transform` property.
+     
+     - Parameter sx: Scale factor in the x dimension
+     - Parameter xy: Scale factor in the y dimension
+     */
+    func scale(sx sx: CGFloat, sy: CGFloat) {
+        self.transform = CGAffineTransformScale(self.transform, sx, sy)
+    }
+    
+    /**
+     Rotate the view using its `transform` property.
+     
+     - Parameter angle: The angle to rotate in radians
+     */
+    func rotate(angle angle: CGFloat) {
+        self.transform = CGAffineTransformRotate(self.transform, angle)
+    }
+    
+    /**
+     Translate the view by altering its `frame` property.
+     
+     - Parameter tx: Translation distance along the x axis
+     - Parameter ty: Translation distance along the y axis
+     */
+    func translateFrame(tx tx: CGFloat, ty: CGFloat) {
+        var frame = self.frame
+        frame.origin.x = frame.origin.x + tx
+        frame.origin.y = frame.origin.y + ty
+        self.frame = frame
+    }
+    
+    /**
+     Stretch or shring the view using its `frame` property.
+     
+     - Parameter deltaWidth: The amount to stretch the frame in the x direction
+     - Parameter deltaHeight: The amount to stretch the frame in the y direction
+     */
+    func stretchFrame(deltaWidth deltaWidth: CGFloat, deltaHeight: CGFloat) {
+        var frame = self.frame
+        frame.size.width = frame.size.width + deltaWidth
+        frame.size.height = frame.size.height + deltaHeight
+        self.frame = frame
+    }
+    
+    /**
+     Resize the view's `frame` property.
+     
+     - Parameter width: The new width
+     - Parameter height: The new height
+     */
+    func resizeFrame(width width: CGFloat, height: CGFloat) {
+        var frame = self.frame
+        frame.size.width = width
+        frame.size.height = height
+        self.frame = frame
+    }
+    
+    /**
+     Flip the view 180 degrees horizontally (along the y axis)
+     
+     - Parameter perspectiveDistance: The simulated distance between the observer
+     and the view, in points. Since the flip is in three dimensions, a perspective
+     transform is applied. Smaller values will have an exaggerated perspective.
+     Default value is 1000 points.
+     */
+    func flipHorizontal(perspectiveDistance: CGFloat = 1000.0) {
+        self.layer.transform.m34 = -1.0 / perspectiveDistance
+        self.layer.transform = CATransform3DRotate(self.layer.transform, CGFloat(M_PI), 0, 1.0, 0)
+    }
+    
+    /**
+     Flip the view 180 degrees vertically (along the x axis)
+     
+     - Parameter perspectiveDistance: The simulated distance between the observer
+     and the view, in points. Since the flip is in three dimensions, a perspective
+     transform is applied. Smaller values will have an exaggerated perspective.
+     Default value is 1000 points.
+     */
+    func flipVertical(perspectiveDistance: CGFloat = 1000.0) {
+        self.layer.transform.m34 = -1.0 / perspectiveDistance
+        self.layer.transform = CATransform3DRotate(self.layer.transform, CGFloat(M_PI), 1.0, 0, 0)
+    }
+    
+    /**
+     Fade in a view by animating its `alpha`.
+     
+     - Parameter duration: The duration of the animation
+     - Returns: An `Animation` that can be chained or altered.
+     */
+    func fadeIn(duration duration: NSTimeInterval = Animation.defaultAnimationDuration) -> Animation {
+        self.alpha = 0.0
+        return animate(duration: duration) {
+            self.alpha = 1.0
+        }
+    }
+    
+    /**
+     Fade out a view by animating its `alpha`.
+     
+     - Parameter duration: The duration of the animation
+     - Returns An `Animation` that can be chained or altered.
+     */
+    func fadeOut(duration duration: NSTimeInterval = Animation.Constants.DefaultAnimationDuration) -> Animation {
+        return animate(duration: duration) {
+            self.alpha = 0.0
         }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+![AnimationSugar](Images/AnimationSugar_logo.png)
+
 ## AnimationSugar
 
 AnimationSugar is a tiny Swift library that can make iOS animation code slightly less ugly.
@@ -8,44 +10,43 @@ It was built at [Intrepid](http://intrepid.io).
 
 ##Examples
 
-    animate(duration: 0.3) {
+    animate {
         view.transform.tx += 150
     }
 
 ![Image of animation to the right](Images/AnimationSugar_1.gif)
 
-    animate(duration: 0.3) {
+    animate {
         view.transform.tx += 150
     }
     .withSpring(dampingRatio: 0.5, initialVelocity: 0)
 
-![Image of animation with spring](Images/AnimationSugar_3.gif)
+![Image of animation with spring](Images/AnimationSugar_2.gif)
 
-    animate(duration: 0.3) {
+    animate {
         view.transform.tx += 150
     }
-    .thenAnimate(duration: 0.3) {
+    .thenAnimate {
         view.transform.ty += 150
     }
-    .thenAnimate(duration: 0.3) {
+    .thenAnimate {
         view.transform.tx -= 150
     }
-    .thenAnimate(duration: 0.3) {
+    .thenAnimate {
         view.transform.ty -= 150
     }
 
-![Image of animation in a square](Images/AnimationSugar_4.gif)
+![Image of animation in a square](Images/AnimationSugar_3.gif)
 
     animate(duration: 0.3) {
         view.transform.tx += 150
     }
     .withOption(.CurveEaseOut)
-    .withCompletion {
-        _ in
-        view.backgroundColor = UIColor.redColor()
+    .withCompletion { _ in
+        view.backgroundColor = UIColor(white: 0.8, alpha: 1.0)
     }
 
-![Image of animation with completion](Images/AnimationSugar_2.gif)
+![Image of animation with completion](Images/AnimationSugar_4.gif)
 
 ## Usage
 
@@ -53,27 +54,26 @@ It was built at [Intrepid](http://intrepid.io).
 
     pod install 'AnimationSugar'
 
-## Methods
+## Reference
 
-- `func animate(duration duration: NSTimeInterval, animations: () -> ()) -> Animation`
-> Starts an animation. Use trailing closure syntax to write your animations, e.g.: `animate(0.3) { ... }`. This is a global method (prevents compiler warnings of unused initializer).
+`func animate(duration duration: NSTimeInterval = defaultAnimationDuration, animations: () -> ()) -> Animation`
 
- 
-- `func withDelay(delay: NSTimeInterval) -> Animation`
-> Set a delay to wait before the animation starts
+- Starts an animation. Use trailing closure syntax to write your animations, e.g.: `animate(duration: 0.3) { ... }`. Without the duration argument, a default duration is used (see `defaultAnimationDuration`). 
 
- 
-- `func withOption(option: UIViewAnimationOptions) -> Animation`
-> Set an animation option for the animation. For multiple options, either chain them e.g. `.withOption(.Autoreverse).withOption(.Repeat)` or use once with an array e.g. `.withOption([.Autoreverse, .Repeat])`.
+`func withDelay(delay: NSTimeInterval) -> Animation`
+- Set a delay to wait before the animation starts.
 
- 
-- `func withSpring(dampingRatio dampingRatio: CGFloat, initialVelocity: CGFloat) -> Animation`
-> Use a spring animation curve with the given damping ratio and initial velocity.
+`func withOption(option: UIViewAnimationOptions) -> Animation`
+- Set an animation option for the animation. For multiple options, either chain them e.g. `.withOption(.Autoreverse).withOption(.Repeat)` or use once with an array e.g. `.withOption([.Autoreverse, .Repeat])`.
 
- 
-- `func withCompletion(completion: (Bool) -> ()) -> Animation`
-> Set a completion block for when the animation finishes. Use trailing closure syntax e.g. `animate(0.3) { ... }.withCompletion { ... }`
+`func withSpring(dampingRatio dampingRatio: CGFloat, initialVelocity: CGFloat) -> Animation`
+- Use a spring animation curve with the given damping ratio and initial velocity.
 
- 
-- `func thenAnimate(duration duration: NSTimeInterval, animations: () -> ()) -> Animation`
-> Chain a new animation to occur after the previous animation is completed. See the "square" example above.
+`func withCompletion(completion: (Bool) -> ()) -> Animation`
+- Set a completion block for when the animation finishes. Use trailing closure syntax e.g. `animate { ... }.withCompletion { ... }`
+
+`func thenAnimate(duration duration: NSTimeInterval = defaultAnimationDuration, animations: () -> ()) -> Animation`
+- Chain a new animation to occur after the previous animation is completed. See the "square" example above. Without the duration argument, a default duration is used (see `defaultAnimationDuration`)
+
+`static var defaultAnimationDuration`
+- Change the default animation duration, to use `animate()` or `thenAnimate()` without a duration argument. This value is initialized to 0.3 seconds and can be changed at any time, i.e. `Animation.defaultAnimationDuration = 0.25`.
